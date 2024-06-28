3ba554be998686aa512605bc50a0cc432abc859e
tejas11054 <140798292+tejas11054@users.noreply.github.com>
2024-06-28 19:04:32 +0530

    Intitial commit

--- src/routes/(tools)/flowchart-maker/+page.svelte
+++ src/routes/(tools)/flowchart-maker/+page.svelte
@@ -4,6 +4,7 @@
 
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
 	<!-- Add tool here -->
+	 <h1>Hello World</h1>
 </div>
 
 <style>
